83049d8e8ae76b1b4035bb795d149970ca5032c9
Craig Hill <craig@Craigs-Home-Mac.fios-router.home>
2020-02-27 21:17:56 -0500

    lab work

---
+++ working-labs/functions.py
@@ -0,0 +1,31 @@
+#def hello():
+#    print("Hello, World!")
+#
+#hello()
+
+""" # Define function names()
+def names():
+    # Set up name variable with input
+    name = str(input('Enter your name: '))
+    # Check whether name has a vowel
+    if set('aeiou').intersection(name.lower()):
+        print('Your name contains a vowel.')
+    else:
+        print('Your name does not contain a vowel.')
+
+    # Iterate over name
+    for letter in name:
+        print(letter) 
+
+# Call the function
+names() """
+
+
+# Functions with parameters
+def add_numbers(x, y, z):
+    a = x + y
+    b = x + z
+    c = y + z
+    print(a, b, c)
+
+add_numbers(1, 2, 3)
